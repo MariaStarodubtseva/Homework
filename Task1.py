277e87d37ee24c68c22456ca4ea0daf7996edfee
Maria Starodubtseva <starodubtseva2@gmail.com>
2021-03-10 22:08:47 +0300

    Домашнее задание урок 1

---
+++ Task1.py
@@ -0,0 +1,25 @@
+a = 10
+print(a)
+
+b = 10.2
+print(b)
+
+c = 'Hello'
+print(c)
+
+d = True
+print(d)
+
+e = [10, 10.2, 'Hello']
+print(e)
+
+f = (10, 10.2, 'Hello')
+print(f)
+
+g = {'name': 'Maria', 'age': 28}
+print(g)
+
+name = input('Как Вас зовут? ')
+age = input('Сколько Вам лет? ')
+occupation = input('Кем Вы работаете? ')
+print('Имя - ', name, 'Возраст - ', age, 'Профессия - ', occupation)
